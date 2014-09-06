2fa2a6083471b45c2ae6d8eae0886233d246dc95
Easimer Daniel <easimer@easimer.tk>
2014-09-06 22:24:28 +0200

    fucked up

--- steampy.cpp
+++ steampy.cpp
@@ -31,24 +31,24 @@
 	}
 #else
 	//Steam
-	bool SteamInit(void);
+	__attribute__ ((visibility ("default"))) bool SteamInit(void);
 	//SteamFriends
-	const char* GetPersonaName(void);
-	int GetFriendCount(void);
-	const char* GetFriendNameByIndex(int index, int flag);
-	int GetFriendStateByIndex(int index, int flag);
-	int GetPersonaState(void);
-	uint32 GetFriendGame(int index, int flag);
-	bool IsFriendInGame(int index, int flag);
-	void SetPersonaName(const char* newname);
+	__attribute__ ((visibility ("default"))) const char* GetPersonaName(void);
+	__attribute__ ((visibility ("default"))) int GetFriendCount(void);
+	__attribute__ ((visibility ("default"))) const char* GetFriendNameByIndex(int index, int flag);
+	__attribute__ ((visibility ("default"))) int GetFriendStateByIndex(int index, int flag);
+	__attribute__ ((visibility ("default"))) int GetPersonaState(void);
+	__attribute__ ((visibility ("default"))) uint32 GetFriendGame(int index, int flag);
+	__attribute__ ((visibility ("default"))) bool IsFriendInGame(int index, int flag);
+	__attribute__ ((visibility ("default"))) void SetPersonaName(const char* newname);
 	//SteamUtils
-	bool IsOverlayEnabled(void);
-	uint8 GetCurrentBatteryPower(void);
-	uint32 GetSecondsSinceAppActive(void);
-	uint32 GetSecondsSinceComputerActive(void);
-	uint32 GetServerRealTime(void);
-	const char* GetIPCountry();
-	bool IsSteamRunningInVR();
+	__attribute__ ((visibility ("default"))) bool IsOverlayEnabled(void);
+	__attribute__ ((visibility ("default"))) uint8 GetCurrentBatteryPower(void);
+	__attribute__ ((visibility ("default"))) uint32 GetSecondsSinceAppActive(void);
+	__attribute__ ((visibility ("default"))) uint32 GetSecondsSinceComputerActive(void);
+	__attribute__ ((visibility ("default"))) uint32 GetServerRealTime(void);
+	__attribute__ ((visibility ("default"))) const char* GetIPCountry();
+	__attribute__ ((visibility ("default"))) bool IsSteamRunningInVR();
 #endif
 
 bool SteamInit()
