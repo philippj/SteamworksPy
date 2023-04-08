a1d5cb030253b417ba5a663b771e242aa8a01fd2
twstagg <tristinstagg@protonmail.com>
2023-04-07 20:59:24 -0500

    Add support when being run from a compiled Nuitka --onefile executable

--- steamworks/__init__.py
+++ steamworks/__init__.py
@@ -7,50 +7,52 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-__version__ = '2.0.0'
-__author__  = 'GP Garcia'
+__version__ = "2.0.0"
+__author__ = "GP Garcia"
 
 import sys, os, time
 from ctypes import *
 from enum import Enum
 
-import steamworks.util 		as steamworks_util
-from steamworks.enums 		import *
-from steamworks.structs 	import *
-from steamworks.exceptions 	import *
-from steamworks.methods 	import STEAMWORKS_METHODS
+import steamworks.util as steamworks_util
+from steamworks.enums import *
+from steamworks.structs import *
+from steamworks.exceptions import *
+from steamworks.methods import STEAMWORKS_METHODS
 
-from steamworks.interfaces.apps         import SteamApps
-from steamworks.interfaces.friends      import SteamFriends
-from steamworks.interfaces.matchmaking  import SteamMatchmaking
-from steamworks.interfaces.music        import SteamMusic
-from steamworks.interfaces.screenshots  import SteamScreenshots
-from steamworks.interfaces.users        import SteamUsers
-from steamworks.interfaces.userstats    import SteamUserStats
-from steamworks.interfaces.utils        import SteamUtils
-from steamworks.interfaces.workshop     import SteamWorkshop
-from steamworks.interfaces.microtxn     import SteamMicroTxn
-from steamworks.interfaces.input        import SteamInput
+from steamworks.interfaces.apps import SteamApps
+from steamworks.interfaces.friends import SteamFriends
+from steamworks.interfaces.matchmaking import SteamMatchmaking
+from steamworks.interfaces.music import SteamMusic
+from steamworks.interfaces.screenshots import SteamScreenshots
+from steamworks.interfaces.users import SteamUsers
+from steamworks.interfaces.userstats import SteamUserStats
+from steamworks.interfaces.utils import SteamUtils
+from steamworks.interfaces.workshop import SteamWorkshop
+from steamworks.interfaces.microtxn import SteamMicroTxn
+from steamworks.interfaces.input import SteamInput
 
-os.environ['LD_LIBRARY_PATH'] = os.getcwd()
+is_nuitka = "__compiled__" in globals()
+
+os.environ["LD_LIBRARY_PATH"] = os.getcwd()
 
 
 class STEAMWORKS(object):
     """
-        Primary STEAMWORKS class used for fundamental handling of the STEAMWORKS API
+    Primary STEAMWORKS class used for fundamental handling of the STEAMWORKS API
     """
+
     _arch = steamworks_util.get_arch()
-    _native_supported_platforms = ['linux', 'linux2', 'darwin', 'win32']
+    _native_supported_platforms = ["linux", "linux2", "darwin", "win32"]
 
     def __init__(self, supported_platforms: list = []) -> None:
         self._supported_platforms = supported_platforms
-        self._loaded 	= False
-        self._cdll 		= None
+        self._loaded = False
+        self._cdll = None
 
-        self.app_id 	= 0
+        self.app_id = 0
 
         self._initialize()
-
 
     def _initialize(self) -> bool:
         """Initialize module by loading STEAMWORKS library
@@ -64,21 +66,42 @@ class STEAMWORKS(object):
         if platform not in STEAMWORKS._native_supported_platforms:
             raise UnsupportedPlatformException(f'"{platform}" is not being supported')
 
-        library_file_name = ''
-        if platform in ['linux', 'linux2']:
-            library_file_name = 'SteamworksPy.so'
-            if os.path.isfile(os.path.join(os.getcwd(), 'libsteam_api.so')):
-                cdll.LoadLibrary(os.path.join(os.getcwd(), 'libsteam_api.so')) #if i do this then linux works
-            elif os.path.isfile(os.path.join(os.path.dirname(__file__), 'libsteam_api.so')):
-                cdll.LoadLibrary(os.path.join(os.path.dirname(__file__), 'libsteam_api.so'))
+        library_file_name = ""
+        if platform in ["linux", "linux2"]:
+            library_file_name = "SteamworksPy.so"
+            if os.path.isfile(os.path.join(os.getcwd(), "libsteam_api.so")):
+                cdll.LoadLibrary(
+                    os.path.join(os.getcwd(), "libsteam_api.so")
+                )  # if i do this then linux works
+            elif os.path.isfile(
+                os.path.join(os.path.dirname(__file__), "libsteam_api.so")
+            ):
+                cdll.LoadLibrary(
+                    os.path.join(os.path.dirname(__file__), "libsteam_api.so")
+                )
+            elif os.path.isfile(
+                os.path.join(
+                    os.path.split(os.path.split(__file__)[0])[0], "libsteam_api.so"
+                )
+            ):
+                cdll.LoadLibrary(
+                    os.path.join(
+                        os.path.split(os.path.split(__file__)[0])[0], "libsteam_api.so"
+                    )
+                )
             else:
-                raise MissingSteamworksLibraryException(f'Missing library "libsteam_api.so"')
+                raise MissingSteamworksLibraryException(
+                    f'Missing library "libsteam_api.so"'
+                )
 
-        elif platform == 'darwin':
-            library_file_name = 'SteamworksPy.dylib'
-
-        elif platform == 'win32':
-            library_file_name = 'SteamworksPy.dll' if STEAMWORKS._arch == Arch.x86 else 'SteamworksPy64.dll'
+        elif platform == "darwin":
+            library_file_name = "SteamworksPy.dylib"
+        elif platform == "win32":
+            library_file_name = (
+                "SteamworksPy.dll"
+                if STEAMWORKS._arch == Arch.x86
+                else "SteamworksPy64.dll"
+            )
 
         else:
             # This case is theoretically unreachable
@@ -88,22 +111,37 @@ class STEAMWORKS(object):
             library_path = os.path.join(os.getcwd(), library_file_name)
         elif os.path.isfile(os.path.join(os.path.dirname(__file__), library_file_name)):
             library_path = os.path.join(os.path.dirname(__file__), library_file_name)
+        elif is_nuitka and os.path.isfile(
+            os.path.join(
+                os.path.split(os.path.split(__file__)[0])[0], library_file_name
+            )
+        ):
+            library_path = os.path.join(
+                os.path.split(os.path.split(__file__)[0])[0], library_file_name
+            )
         else:
-            raise MissingSteamworksLibraryException(f'Missing library {library_file_name}')
-
-        app_id_file = os.path.join(os.getcwd(), 'steam_appid.txt')
+            raise MissingSteamworksLibraryException(
+                f"Missing library {library_file_name}"
+            )
+        if is_nuitka and os.path.join(
+            os.path.split(os.path.split(__file__)[0])[0], "steam_appid.txt"
+        ):
+            app_id_file = os.path.join(
+                os.path.split(os.path.split(__file__)[0])[0], "steam_appid.txt"
+            )
+        else:
+            app_id_file = os.path.join(os.getcwd(), "steam_appid.txt")
         if not os.path.isfile(app_id_file):
-            raise FileNotFoundError(f'steam_appid.txt missing from {os.getcwd()}')
+            raise FileNotFoundError(f"steam_appid.txt missing from {os.getcwd()}")
 
-        with open(app_id_file, 'r') as f:
-            self.app_id	= int(f.read())
+        with open(app_id_file, "r") as f:
+            self.app_id = int(f.read())
 
-        self._cdll 		= CDLL(library_path) # Throw native exception in case of error
-        self._loaded 	= True
+        self._cdll = CDLL(library_path)  # Throw native exception in case of error
+        self._loaded = True
 
         self._load_steamworks_api()
         return self._loaded
-
 
     def _load_steamworks_api(self) -> None:
         """Load all methods from steamworks api and assign their correct arg/res types based on method map
@@ -111,39 +149,37 @@ class STEAMWORKS(object):
         :return: None
         """
         if not self._loaded:
-            raise SteamNotLoadedException('STEAMWORKS not yet loaded')
+            raise SteamNotLoadedException("STEAMWORKS not yet loaded")
 
         for method_name, attributes in STEAMWORKS_METHODS.items():
             f = getattr(self._cdll, method_name)
 
-            if 'restype' in attributes:
-                f.restype = attributes['restype']
+            if "restype" in attributes:
+                f.restype = attributes["restype"]
 
-            if 'argtypes' in attributes:
-                f.argtypes = attributes['argtypes']
+            if "argtypes" in attributes:
+                f.argtypes = attributes["argtypes"]
 
             setattr(self, method_name, f)
 
         self._reload_steamworks_interfaces()
-
 
     def _reload_steamworks_interfaces(self) -> None:
         """Reload all interface classes
 
         :return: None
         """
-        self.Apps           = SteamApps(self)
-        self.Friends        = SteamFriends(self)
-        self.Matchmaking    = SteamMatchmaking(self)
-        self.Music          = SteamMusic(self)
-        self.Screenshots    = SteamScreenshots(self)
-        self.Users          = SteamUsers(self)
-        self.UserStats      = SteamUserStats(self)
-        self.Utils          = SteamUtils(self)
-        self.Workshop       = SteamWorkshop(self)
-        self.MicroTxn       = SteamMicroTxn(self)
-        self.Input          = SteamInput(self)
-
+        self.Apps = SteamApps(self)
+        self.Friends = SteamFriends(self)
+        self.Matchmaking = SteamMatchmaking(self)
+        self.Music = SteamMusic(self)
+        self.Screenshots = SteamScreenshots(self)
+        self.Users = SteamUsers(self)
+        self.UserStats = SteamUserStats(self)
+        self.Utils = SteamUtils(self)
+        self.Workshop = SteamWorkshop(self)
+        self.MicroTxn = SteamMicroTxn(self)
+        self.Input = SteamInput(self)
 
     def initialize(self) -> bool:
         """Initialize Steam API connection
@@ -151,21 +187,23 @@ class STEAMWORKS(object):
         :return: bool
         """
         if not self.loaded():
-            raise SteamNotLoadedException('STEAMWORKS not yet loaded')
+            raise SteamNotLoadedException("STEAMWORKS not yet loaded")
 
         if not self.IsSteamRunning():
-            raise SteamNotRunningException('Steam is not running')
+            raise SteamNotRunningException("Steam is not running")
 
         # Boot up the Steam API
         result = self._cdll.SteamInit()
         if result == 2:
-            raise SteamNotRunningException('Steam is not running')
+            raise SteamNotRunningException("Steam is not running")
 
         elif result == 3:
-            raise SteamConnectionException('Not logged on or connection to Steam client could not be established')
+            raise SteamConnectionException(
+                "Not logged on or connection to Steam client could not be established"
+            )
 
         elif result != 0:
-            raise GenericSteamException('Failed to initialize STEAMWORKS API')
+            raise GenericSteamException("Failed to initialize STEAMWORKS API")
 
         return True
 
@@ -183,17 +221,15 @@ class STEAMWORKS(object):
         :return: None
         """
         self._cdll.SteamShutdown()
-        self._loaded    = False
-        self._cdll      = None
-
+        self._loaded = False
+        self._cdll = None
 
     def loaded(self) -> bool:
         """Is library loaded and everything populated
 
         :return: bool
         """
-        return (self._loaded and self._cdll)
-
+        return self._loaded and self._cdll
 
     def run_callbacks(self) -> bool:
         """Execute all callbacks
@@ -201,7 +237,7 @@ class STEAMWORKS(object):
         :return: bool
         """
         if not self.loaded():
-            raise SteamNotLoadedException('STEAMWORKS not yet loaded')
+            raise SteamNotLoadedException("STEAMWORKS not yet loaded")
 
         self._cdll.RunCallbacks()
         return True
